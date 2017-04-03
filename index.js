32d7411469bc41ef8743f3fc1beb30ba8e662260
Hyukpyo Hong <hyukpyohong@gmail.com>
2017-04-03 05:57:02 -0400

    Login, Logout, Register logic Developed

--- index.js
+++ index.js
@@ -1,28 +1,186 @@
+// Using Express engine
 var express = require('express');
+var session = require('express-session');
+//var MySQLStore = require('express-mysql-session')(session);
 var app = express();
-app.locals.pretty = true;
+
+// To process post request
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//View Engine
 app.set('view engine', 'pug');
 app.set('views', './src/views');
-app.use(express.static('./src/resources')); // using static file 
-var bodyParser = require('body-parser'); // for recieve post request
-app.use(bodyParser.urlencoded({ extended: false }));
-var port = 4321
-app.listen(port,()=>{
-    console.log('Connected to port: '+port);
-})
+app.use(express.static('./src/resources')); // using static file from here.
 
-app.get('/', (req,res)=>{
+// DB connection
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'dbms.gswcm.net',
+    user: 'hhong',
+    password: '219590',
+    database: 'Group 2' // Real DB
+    //database : 'Hhong' // Test DB
+});
+conn.connect();
+
+//Session Managing on file
+app.use(session({
+    secret: 'ad23kdjfajajf',
+    resave: false,
+    saveUninitialized: true,
+
+    /* in case of managing MYSQL
+    secret: '1234DSFs@adf1234!@#$asd',
+    resave: false,
+    saveUninitialized: true,
+    store: new MySQLStore({
+        host: 'dbms.gswcm.net',
+        port: 3306,
+        user: 'hhong',
+        password: '219590',
+        database: 'Group 2'
+    })
+    */
+}));
+
+
+// Passport API for Authentication
+// PBKDF2 for cryptographic function
+var bkfd2Password = require("pbkdf2-password");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var hasher = bkfd2Password();
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    console.log('SerializeUser func passed', user.mem_email)
+    done(null, user.mem_email);
+});
+passport.deserializeUser(function (id, done) {
+    console.log('deserializeUser', id);
+    var sql = 'select * from MEMBER where mem_email=?';
+    conn.query(sql, [id], (err, results) => {
+        if (err) {
+            console.log(err);
+            done('There is no Users');
+        } else {
+            done(null, results[0]);
+        }
+    })
+});
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        console.log('Enter Strategy ', username)
+        var sql = 'select * from MEMBER where mem_email=?';
+        conn.query(sql, [username], (err, results) => {
+            if (err) {
+                return done('There is no users');
+            }
+            var user = results[0];
+            return hasher({ password: password, salt: user.salt }, (err, pass, salt, hash) => {
+                if (hash === user.mem_pw) {
+                    console.log('Password correct');
+                    done(null, user);
+                } else {
+                    done(null, false);
+                }
+            })
+        })
+    }
+));
+
+
+
+
+
+// --------------------------------------------Router Start ---------------------------------------------------------------
+//Initial Page
+app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.post('/main', (req,res)=>{
-    res.render('main')
-})
+//Sign in Process
+app.post('/signin', passport.authenticate(
+    'local',
+    {
+        successRedirect: '/main',
+        failureRedirect: '/',
+        failureFlash: false
+    }
+));
 
-app.post('/signup', (req,res)=>{
-    res.render('main')
-})
+// After sucess Sign in
+app.get('/main', (req, res) => {
+    if (req.user !== undefined) {
+        res.render('main', {
+            id: req.user.mem_email,
+            major: req.user.deg_major,
+            level: req.user.deg_level,
+            startyear: req.user.mem_startyear,
+        })
+    } else {
+        res.redirect('/');
+    }
+});
 
-app.get('/findpw', (req,res)=>{
+//Signup Process
+app.post('/signup', (req, res) => {
+    hasher({ password: req.body.password }, function (err, pass, salt, hash) {
+        var user = {
+            mem_email: req.body.mem_email,
+            mem_pw: hash,
+            mem_startyear: req.body.mem_startyear,
+            deg_level: req.body.deg_level,
+            deg_major: req.body.deg_major,
+            salt: salt,
+        };
+        var sql = 'INSERT INTO MEMBER SET ?';
+        conn.query(sql, user, function (err, results) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+            } else {
+                req.login(user, (err) => {
+                    console.log('Login Sucess');
+                    req.session.save(() => {
+                        res.redirect('/main');
+                    })
+                })
+
+            }
+        });
+    });
+});
+
+
+//Finding password page
+app.get('/findpw', (req, res) => {
     res.render('findpw')
+})
+
+//Finding password process
+app.post('/findpw', (req, res) => {
+    res.render('findpw')
+})
+
+// Logout
+app.post('/logout', function (req, res) {
+    req.logout();
+    req.session.save(function () {
+        res.redirect('/');
+    });
+});
+
+// --------------------------------------------Router End---------------------------------------------------------------
+
+
+
+
+
+//Server Launch
+var port = 4321
+app.listen(port, () => {
+    console.log('Connected to port: ' + port);
 })
