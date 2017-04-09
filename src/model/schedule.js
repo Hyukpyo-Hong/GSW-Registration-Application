a7ca5ff3a5549fa488d7d80b679014128fe2a21e
Hyukpyo Hong <hyukpyohong@gmail.com>
2017-04-08 23:19:08 -0400

    Apply Bootstrap

--- src/model/schedule.js
+++ src/model/schedule.js
@@ -9,10 +9,7 @@ exports.getSchedule = (conn, year, semester) => {
 
             } else {
                 console.log(year, " ", semester, " schedule loading success");
-                var html =`<div>` + year + ' ' + semester + ` Schedule</div>`;
-                html += `<div>CRN	SUBJ CODE	CRSE NO.	TITLE	PTRM	CRED HOURS	SEATS AVAIL	TOTAL SEATS	DAYS	CLASS TIME	LOCATION	INSTRUCTOR<div>`;
-                html += makeHtml(rows);
-
+                var html = makeHtml(rows);
                 resolve(html);
             }
         });
@@ -20,9 +17,32 @@ exports.getSchedule = (conn, year, semester) => {
 }
 
 function makeHtml(rows) {
-    var html;
+    var html = '';
+    html += '<table class="table table-hover">'
+    html += '<thead>'
+    html += '<th>CRN</th>'
+    html += '<th>SUBJ. CRSE</th>'    
+    html += '<th>TITLE</th>'
+    html += '<th>PTRM/<br/>CREDIT</th>'
+    html += '<th>Avai./<br>Total</th>'
+    html += '<th>DAYS/TIME</th>'
+    html += '<th>LOCATION</th>'
+    html += '<th>Instructor</th>';
+    html += '</thead>'
+    html += '<tbody>'
     for (var i in rows) {
-        html += `<div>` + rows[i].cl_Title + `</div>`;
+        html += `<tr>`;
+        html += '<td>' + rows[i].cl_CRN + '</td>'
+        html += '<td>' + rows[i].cl_SubjCode +' '+rows[i].cl_CrseNo + '</td>'
+        html += '<td>' + rows[i].cl_Title + '</td>'
+        html += '<td>' + rows[i].cl_PTRM +' / '+rows[i].cl_CreditHours+ '</td>'
+        html += '<td>' + rows[i].cl_SeatAvail + '/' + rows[i].cl_TotalSeats + '</td>'
+        html += '<td>' + rows[i].cl_Days +((rows[i].cl_start_time === '00:00:00') ? '' : ' / '+rows[i].cl_start_time.substring(0, 5) + '-' + rows[i].cl_end_time.substring(0, 5))+'</td>'
+        html += '<td>' + rows[i].cl_Location + '</td>'
+        html += '<td>' + rows[i].cl_instructor + '</td>'
+        html += `</tr>`;
     }
+    html += '</tbody>'
+    html += '</table>'
     return html;
 }
