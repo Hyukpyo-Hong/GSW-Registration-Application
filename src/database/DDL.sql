370a9ab9a9087e37bb2c61734d6aac3ea1888db0
Hyukpyo Hong <hyukpyohong@gmail.com>
2017-04-19 14:40:41 -0400

    DML Query Update

--- src/database/DDL.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE description(
-    cl_CrseNo smallint(6) not null,
-    cl_SubjCode varchar(20) not null,
-    des_contents varchar(500) not null,
-    CONSTRAINT PK_description primary key (cl_CrseNo,cl_SubjCode)
-);
-
--- src/model/under_computerscience.js
+++ src/model/under_computerscience.js
@@ -87,6 +87,7 @@ exports.getCurriculum = (connector) => {
 function getArea(area, group) {
     let rows = (filter(totalrows, 'curi_area', area));
     rows = (filter(rows, 'curi_group', group));
+    console.log(rows);
     var html = "<thead><th colspan='3' id='class_group'>" + "↳Select '" + rows[0].cg_credit + "' Credits</th></thead>"
     for (var i in rows) {
         let code = rows[i].cl_SubjCode + ' ' + rows[i].cl_CrseNo;
@@ -95,61 +96,5 @@ function getArea(area, group) {
         html += '<td id="title"><strong> ' + rows[i].cl_Title + '</strong> (' + rows[i].cl_CreditHours + ')</td>';
         html += '</tr>'
     }
-    return html;
-}
-
-
-
-/*
- 
-  curi_year: 2016-2017
-  deg_level: Undergraduate
-  deg_major: ComputerScience
-cl_SubjCode: WGSS
-  cl_CrseNo: 2001
-  curi_area: b
- curi_group: 1
- 
-        var sql = "select * from course_list left join course using (cl_SubjCode, cl_CrseNo) where cl_close!=? and cl_year=? and cl_semester=? order by cl_SubjCode, cl_CrseNo";
-        var params = ['c', year, semester];
-        conn.query(sql, params, function (err, rows) {
-            if (err) {
-                console.log(err);
-                reject(err);
-    
-            } else {
-                console.log(year, " ", semester, " schedule loading success");
-                var html = makeHtml(rows);
-                resolve(html);
-            }
-        });*/
-function makeHtml(rows) {
-    var html = '';
-    html += '<table class="table table-hover" id="scheduleTable">'
-    html += '<thead>'
-    html += '<th>PTRM</th>'
-    html += '<th>CRN</th>'
-    html += '<th>SUBJ CRSE</th>'
-    html += '<th>TITLE (Credit Hours)</th>'
-    html += '<th>Avai/<br>Total</th>'
-    html += '<th>DAYS/TIME</th>'
-    html += '<th>LOCATION</th>'
-    html += '<th>Instructor</th>';
-    html += '</thead>'
-    html += '<tbody>'
-    for (var i in rows) {
-        html += '<tr>'
-        html += '<td>' + rows[i].cl_PTRM + '</td>'
-        html += '<td>' + rows[i].cl_CRN + '</td>'
-        html += '<td>' + rows[i].cl_SubjCode + ' ' + rows[i].cl_CrseNo + '</td>'
-        html += '<td id="title">' + rows[i].cl_Title + " (" + rows[i].cl_CreditHours + ')</td>';
-        html += '<td>' + rows[i].cl_SeatAvail + '/' + rows[i].cl_TotalSeats + '</td>'
-        html += '<td>' + rows[i].cl_Days + ((rows[i].cl_start_time === '00:00:00') ? '' : ' / ' + rows[i].cl_start_time.substring(0, 5) + '-' + rows[i].cl_end_time.substring(0, 5)) + '</td>'
-        html += '<td>' + rows[i].cl_Location + '</td>'
-        html += '<td>' + rows[i].cl_instructor + '</td>'
-        html += `</tr>`;
-    }
-    html += '</tbody>'
-    html += '</table>'
     return html;
 }
